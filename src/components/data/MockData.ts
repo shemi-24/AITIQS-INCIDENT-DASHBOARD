295c643e3ceaa3e8b8a0b6e07ecb59672399452b
shemeer <shemeerummer24@gmail.com>
2025-05-16 11:29:01 +0530

    Stream IP updated

--- src/components/data/MockData.ts
+++ src/components/data/MockData.ts
@@ -4,105 +4,105 @@ export const MOCK_STREAMS=[
     id:'1',
     title:'Fall Detection',
     description:"fall detction is very dangerous",
-    streamUrl:'http://192.168.5.46:8000/face_and_fall/api/detect',
-    startUrl:'http://192.168.5.46:8000/face_and_fall/api/detect',
-    stopUrl:'http://192.168.5.46:8000/face_and_fall/api/stop',
+    streamUrl:'http://192.168.5.105:8000/face_and_fall/api/detect',
+    startUrl:'http://192.168.5.105:8000/face_and_fall/api/detect',
+    stopUrl:'http://192.168.5.105:8000/face_and_fall/api/stop',
 
   },
   {
     id:'2',
     title:'PPE kit Detection',
-    streamUrl:'http://192.168.5.46:8001/ppekit/api/detect',
-    startUrl:'http://192.168.5.46:8001/ppekit/api/detect',
-    stopUrl:'http://192.168.5.46:8001/ppekit/api/stop'
+    streamUrl:'http://192.168.5.105:8001/ppekit/api/detect',
+    startUrl:'http://192.168.5.105:8001/ppekit/api/detect',
+    stopUrl:'http://192.168.5.105:8001/ppekit/api/stop'
   },
   {
     id:'3',
     title:'Vehicle Accident Detection',
-    streamUrl:'http://192.168.5.46:8002/accident/api/detect',
-    startUrl:'http://192.168.5.46:8002/accident/api/detect',
-    stopUrl:'http://192.168.5.46:8002/accident/api/stop'
+    streamUrl:'http://192.168.5.105:8002/accident/api/detect',
+    startUrl:'http://192.168.5.105:8002/accident/api/detect',
+    stopUrl:'http://192.168.5.105:8002/accident/api/stop'
     
   },
   {
     id:'4',
     title:'Violence Detection ',
     description:"car parking is very simple",
-    streamUrl:'http://192.168.5.46:8003/violence/api/detect',
-    startUrl:'http://192.168.5.46:8003/violence/api/detect',
-    stopUrl:'http://192.168.5.46:8003/violence/api/stop',
+    streamUrl:'http://192.168.5.105:8003/violence/api/detect',
+    startUrl:'http://192.168.5.105:8003/violence/api/detect',
+    stopUrl:'http://192.168.5.105:8003/violence/api/stop',
 
   },
   {
     id:'5',
     title:'Face Mask Detection ',
     description:"Detection for facr mask",
-    streamUrl:'http://192.168.5.46:8004/face-mask/api/detect',
-    startUrl:'http://192.168.5.46:8004/face-mask/api/detect',
-    stopUrl:'http://192.168.5.46:8004/face-mask/api/stop',
+    streamUrl:'http://192.168.5.105:8004/face-mask/api/detect',
+    startUrl:'http://192.168.5.105:8004/face-mask/api/detect',
+    stopUrl:'http://192.168.5.105:8004/face-mask/api/stop',
 
   },
   {
     id:'6',
     title:'Fire Detection ',
     description:"Detection for fire",
-    streamUrl:'http://192.168.5.46:8005/fire_and_smoke/api/detect',
-    startUrl:'http://192.168.5.46:8005/fire_and_smoke/detect',
-    stopUrl:'http://192.168.5.46:8005/fire_and_smoke/api/stop',
+    streamUrl:'http://192.168.5.105:8005/fire_and_smoke/api/detect',
+    startUrl:'http://192.168.5.105:8005/fire_and_smoke/detect',
+    stopUrl:'http://192.168.5.105:8005/fire_and_smoke/api/stop',
 
   },
   {
     id:'7',
     title:'Forklift Detection ',
     description:"Detection for fire",
-    streamUrl:'http://192.168.5.46:8006/forklift/api/detect',
-    startUrl:'http://192.168.5.46:8006/forklift/api/detect',
-    stopUrl:'http://192.168.5.46:8006/forklift/api/stop',
+    streamUrl:'http://192.168.5.105:8006/forklift/api/detect',
+    startUrl:'http://192.168.5.105:8006/forklift/api/detect',
+    stopUrl:'http://192.168.5.105:8006/forklift/api/stop',
 
   },
   {
     id:'8',
     title:'Hardhat Detection ',
     description:"Detection for fire",
-    streamUrl:'http://192.168.5.46:8007/hardhat/api/detect',
-    startUrl:'http://192.168.5.46:8007/hardhat/api/detect',
-    stopUrl:'http://192.168.5.46:8007/hardhat/api/stop',
+    streamUrl:'http://192.168.5.105:8007/hardhat/api/detect',
+    startUrl:'http://192.168.5.105:8007/hardhat/api/detect',
+    stopUrl:'http://192.168.5.105:8007/hardhat/api/stop',
 
   },
   {
     id:'9',
     title:'Employee Count Detection ',
     description:"Detection for fire",
-    streamUrl:'http://192.168.5.46:8008/max_count/api/detect',
-    startUrl:'http://192.168.5.46:8008/max_count/api/detect',
-    stopUrl:'http://192.168.5.46:8008/max_count/api/stop',
+    streamUrl:'http://192.168.5.105:8008/max_count/api/detect',
+    startUrl:'http://192.168.5.105:8008/max_count/api/detect',
+    stopUrl:'http://192.168.5.105:8008/max_count/api/stop',
 
   },
   {
     id:'10',
     title:'Security Person Detection ',
     description:"Detection for fire",
-    streamUrl:'http://192.168.5.46:8009/security/api/detect',
-    startUrl:'http://192.168.5.46:8009/security/api/detect',
-    stopUrl:'http://192.168.5.46:8009/security/api/stop',
+    streamUrl:'http://192.168.5.105:8009/security/api/detect',
+    startUrl:'http://192.168.5.105:8009/security/api/detect',
+    stopUrl:'http://192.168.5.105:8009/security/api/stop',
 
   },
   {
     id:'11',
     title:'Social Distancing Detection ',
     description:"Detection for fire",
-    streamUrl:'http://192.168.5.46:8010/social_distance/api/detect',
-    startUrl:'http://192.168.5.46:8010/social_distance/api/detect',
-    stopUrl:'http://192.168.5.46:8010/social_distance/api/stop',
+    streamUrl:'http://192.168.5.105:8010/social_distance/api/detect',
+    startUrl:'http://192.168.5.105:8010/social_distance/api/detect',
+    stopUrl:'http://192.168.5.105:8010/social_distance/api/stop',
 
   },
   {
     id:'12',
     title:'Weapon Detection ',
     description:"Detection for fire",
-    streamUrl:'http://192.168.5.46:8011/weapon/api/detect',
-    startUrl:'http://192.168.5.46:8011/weapon/api/detect',
-    stopUrl:'http://192.168.5.46:8011/weapon/api/stop',
+    streamUrl:'http://192.168.5.105:8011/weapon/api/detect',
+    startUrl:'http://192.168.5.105:8011/weapon/api/detect',
+    stopUrl:'http://192.168.5.105:8011/weapon/api/stop',
 
   },
   
